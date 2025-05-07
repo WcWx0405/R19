1ba455a6ff66962625e5f320d1df0f98775ff17f
William1 <william1@misoft.io>
2025-05-07 20:36:29 +0800

    done

--- aifit/convex/schema.ts
+++ aifit/convex/schema.ts
@@ -11,11 +11,11 @@ export default defineSchema({
     }).index("by_clerk_id", ["clerkId"]),
 
     plans: defineTable({
-        userId: v.id("users"),
+        userId: v.string(),
         name: v.string(),
         workoutPlan: v.object({
             schedule: v.array(v.string()),
-            exercise: v.array(v.object({
+            exercises: v.array(v.object({
                 day: v.string(),
                 routines: v.array(v.object({
                     name: v.string(),
@@ -32,7 +32,7 @@ export default defineSchema({
             dailyCalories: v.number(),
             meals: v.array(v.object({
                 name: v.string(),
-                food: v.array(v.string())
+                foods: v.array(v.string())
             }))
         }),
 
